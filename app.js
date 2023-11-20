aee3ec3b14c2a048bf1fd31b24d505b58882835c
nxmoulleb <nxmoulleb@gmail.com>
2023-11-20 15:18:15 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+import "dotenv/config";
+import express from "express";
+import Lab5 from "./lab5.js";
+import CourseRoutes from "./courses/routes.js";
+import ModuleRoutes from "./modules/routes.js";
+import cors from "cors";
+const app = express();
+app.use(cors());
+app.use(express.json());
+ModuleRoutes(app);
+CourseRoutes(app);
+Lab5(app);
+app.listen(process.env.PORT || 4000);
